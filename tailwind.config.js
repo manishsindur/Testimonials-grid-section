e74d30c6507c094fb63782d8042f94e4be3009b9
Manish Sindur <manishsindur@gmail.com>
2024-04-26 16:20:45 +0530

    Update TailwindCSS configuration with new font and color settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Barlow_Semi_Condensed: ['"Barlow Semi Condensed", sans-serif'],
+      },
+      colors: {
+        Light_Gray: "#CFCFCF",
+        Light_Grayish_Blue: "#F1F8FC",
+        Very_dark_grayish_blue: "#4a5965",
+        Very_dark_blackish_blue: "#1d2731",
+        Moderate_violet: "#8551A1",
+      },
+    },
   },
   plugins: [],
 };
